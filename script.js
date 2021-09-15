1df074f075beeb0837e39de0266ee773a574d46e
ancagi <ancagiraldo@gmail.com>
2021-09-15 18:53:51 -0500

    Script file modified

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function getShortMessages(mesages){
-    var result =mesages.map(function(x){
-        return x.message
-    }).filter(function(mesage){
-        return mesage.length <50 
-    })
-    return result
-}
-module.exports= getShortMessages
 
 
 
